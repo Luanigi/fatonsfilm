5040074649203e92f7a49b7a92479179f141d309
Luan <89736491+Luanigi@users.noreply.github.com>
2024-11-15 23:39:28 +0100

    header fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -108,7 +108,7 @@ export default function Header() {
             isSidebarOpen ? 'scale-100' : 'scale-0'
           }   transition-transform duration-300 ease-in-out translate-x-0 z-[-2]`}
         >
-          <nav className="flex flex-col items-center space-y-4 py-4 ">
+          <nav className="flex flex-col items-center space-y-4 py-4 mt-3 w-10/12 mx-auto rounded-lg backdrop-blur-lg bg-black/20">
             <Link href="/" className='hover:bg-black/30 w-5/6 text-center p-3 drop-shadow-2xl rounded-lg bg-black/20'>
               <p className="hover:text-gray-300  transition duration-300" onClick={toggleSidebar}>Home</p>
             </Link>
